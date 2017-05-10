aeda0f09f33e6352f2361bc026c66360fa260f92
ralexand56 <ralexand56@live.com>
2017-05-10 02:10:44 -0700

    Add files via upload

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import AppStyle from './AppStyle';
+
+const App = () => (
+  <div style={AppStyle}>
+    <h2>Hello, Rico! Why are you doing this?</h2>
+  </div>
+);
+
+export default App;
---
+++ src/components/AppStyle.tsx
@@ -0,0 +1,7 @@
+const AppStyle = {
+  fontFamily: 'helvetica, arial, sans-serif',
+  lineHeight: '200%',
+  padding: '6px 20px 30px',
+}
+
+export default AppStyle
